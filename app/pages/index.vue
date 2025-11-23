dfec4950225f978a779bdf8bb2a644d265479920
TusharW4ni <reachtusharwani@gmail.com>
2025-11-23 15:29:35 -0600

    added logout functionality

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,14 @@
+<script setup lang="ts">
+import {authClient} from "../utils/auth-client"
+
+async function logout() {
+	await authClient.signOut();
+	return navigateTo("/auth")
+}
+</script>
+
 <template>
+	<button @click="logout">logout</button>
 	<div>
 		Signed in
 	</div>
